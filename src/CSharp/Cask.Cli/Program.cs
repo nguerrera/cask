11524b21ea89ba399e7013f2fc6bf84f353b505c
Michael C. Fanning <mikefan@microsoft.com>
2024-11-21 14:11:23 -0800

    Restore key generation.

--- src/CSharp/Cask.Cli/Program.cs
+++ src/CSharp/Cask.Cli/Program.cs
@@ -9,22 +9,22 @@ namespace CommonAnnotatedSecurityKeys.Cli
         [STAThread]
         public static int Main(string[] args)
         {
-            //try
-            //{
-            //    return Parser.Default.ParseArguments<
-            //        GenerateOptions,
-            //        ValidateOptions
-            //        >(args)
-            //      .MapResult(
-            //        (GenerateOptions options) => new GenerateCommand().Run(options),
-            //        (ValidateOptions options) => new ValidateCommand().Run(options),
-            //        _ => 1);
-            //}
-            //catch (Exception e)
-            //{
-            //    Console.WriteLine(e);
+            try
+            {
+                return Parser.Default.ParseArguments<
+                    GenerateOptions,
+                    ValidateOptions
+                    >(args)
+                  .MapResult(
+                    (GenerateOptions options) => new GenerateCommand().Run(options),
+                    (ValidateOptions options) => new ValidateCommand().Run(options),
+                    _ => 1);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
                 return 1;
-            //}
+            }
         }
     }
 }
