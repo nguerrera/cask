639be3a154e59e706bc81db28b9a5b0b779b3979
Nick Guerrera <nguerrera@microsoft.com>
2025-04-17 15:17:13 -0500

    Remove C++ implementation stub (#69)
    We will add this back when we're actually ready to work on the C++
    implementation.

--- src/libcask/cask_cimport_test.c
+++
@@ -1,5 +0,0 @@
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-// Dummy file to validate cask.h can be #included from C
-#include "cask.h"
